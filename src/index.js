a7b15bb7e3497324eecaf2b92748f4cf8b5934e3
Vlad Semenik <vlad.semenik@gmail.com>
2018-02-22 22:59:49 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,65 @@
 class Sorter {
   constructor() {
-    // your implementation
+      this.arr = [];
+      this.type_sort =0;
   }
 
   add(element) {
-    // your implementation
+      this.element = element;
+      this.arr.splice(this.arr.length, 0, this.element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+      return this.arr.length;
   }
-
   toArray() {
-    // your implementation
+      return this.arr;
   }
-
   sort(indices) {
-    // your implementation
+      this.indices = indices;
+      this.indices.sort(function(a, b) {
+            return a - b;
+        });
+    var mas= [];
+      for(var i=0; i<this.indices.length; i++){
+          mas.splice(mas.length, 0, this.arr[this.indices[i]]);
+      }
+      
+      switch(this.type_sort){
+        case 1: { mas.sort(function(a, b) {
+                return a.age - b.age;
+            });
+      
+                }
+      case 2: {
+          mas.sort(function(a, b) {
+                return b - a;
+            });}
+                        
+      default: {
+            mas.sort(function(a, b) {
+            return a - b;
+        }); 
+    }
   }
-
+        
+      for(var i=0; i<this.indices.length; i++){
+          this.arr[this.indices[i]]=mas[i];
+      }
+      return this.arr;
+  }
+  
   setComparator(compareFunction) {
-    // your implementation
+      switch(compareFunction){
+          case 'AGE_COMPARATOR': this.type_sort =1; break;
+          case 'REVERSE_COMPARATOR': this.type_sort =2; break;
+          case 'STRINGIFY_COMPARATOR': this.type_sort =3; break;
+      }
+      return ;
   }
 }
 
